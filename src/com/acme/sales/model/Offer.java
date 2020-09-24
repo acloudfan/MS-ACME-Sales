bf95567aab70036b2f0d534da18f9fdbe4b89ab3
acloudfan <raj@acloudfan.com>
2020-09-24 07:05:09 -0400

    ok

---
+++ src/com/acme/sales/model/Offer.java
@@ -0,0 +1,42 @@
+package com.acme.sales.model;
+
+import java.util.Date;
+
+/**
+ * Strategic Pattern: Value Object
+ * Model: Acme Sales
+ * Represents a discount offer that is applied to the Vacation Product
+ */
+
+public class Offer {
+
+    // Some kind of reference code - Not an identity
+    public final String reference;
+
+    // Represents Percentage between 0 & 100
+    public final int discount;
+
+    // Description of the discount
+    public final String description;
+
+    // Valid Till this date
+    public final Date validTill;
+
+    /** Constructor **/
+    public Offer(String reference, int discount, String description, Date validTill) {
+        this.reference = reference;
+        this.discount = discount;
+        this.description = description;
+        this.validTill = validTill;
+    }
+
+    /** Behavior **/
+
+    /**
+     * This applies the offer to the Vacation Product
+     * @return product price after the calculations
+     */
+    public double applyOffer(Proposal proposal){
+        return 0.0;
+    }
+}
