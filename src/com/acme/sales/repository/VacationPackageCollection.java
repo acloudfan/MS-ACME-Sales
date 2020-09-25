d33bd0bb2062e4da738c6b2202a284f15c28abf8
acloudfan <raj@acloudfan.com>
2020-09-25 07:47:52 -0400

    ok

---
+++ src/com/acme/sales/repository/VacationPackageCollection.java
@@ -0,0 +1,24 @@
+package com.acme.sales.repository;
+
+import com.acme.sales.model.VacationPackage;
+
+import java.util.List;
+
+/**
+ * Pattern: Repository
+ * Model: Acme Sales
+ * Represents a collection of VacationPackage
+ */
+
+public interface VacationPackageCollection {
+
+    // Adds a vacation package to the persistent store
+    int  addVacationPackage(VacationPackage vacationPackage);
+
+    // Get a vacation package by name
+    VacationPackage getVacationPackage(String name);
+
+    // Lookup vacation packages
+    List<VacationPackage> findVacationPackage(String destination, int vacationType, );
+
+}
---
+++ src/com/acme/sales/repository/dao/VacationPackageDAO.java
@@ -0,0 +1,14 @@
+package com.acme.sales.repository.dao;
+
+import com.acme.sales.model.VacationPackage;
+
+import java.util.List;
+
+/**
+ * Pattern: Data Access Object
+ * Contains the logic for interactions with underlying database
+ */
+public interface VacationPackageDAO {
+
+
+}
